3d1a05812e002aff22daaedc6764fcaf610c54f8
Tejas Falke <75989281+tedy-art@users.noreply.github.com>
2023-12-12 10:04:00 +0530

    update project

--- my_tennis_club/members/models.py
+++ my_tennis_club/members/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 # Create your models here.
 class Member(models.Model):
     firstname = models.CharField(max_length=225)
--- my_tennis_club/members/views.py
+++ my_tennis_club/members/views.py
@@ -1,8 +1,38 @@
 from django.http import HttpResponse
 from django.template import loader
+from .models import Member
 
 
 # Create your views here.
 def members(request):
-    template = loader.get_template("myfirst.html")
-    return HttpResponse(template.render())
+    """
+    - Creates a mymembers object with all the values of the Member model.
+    - Loads the all_members.html template.
+    - Creates an object containing the mymembers object.
+    - Sends the object to the template.
+    - Outputs the HTML rendered by the template.
+    """
+    mymembers = Member.objects.all().values()
+    template = loader.get_template("all_members.html")
+    context = {
+        'mymembers': mymembers,
+    }
+    return HttpResponse(template.render(context, request))
+
+
+def details(request, id):
+    """
+    The details view does the following:
+        - Gets the id as an argument.
+        - Uses the id to locate the correct record in the Member table.
+        - loads the details.html template.
+        - Creates an object containing the member.
+        - Sends the object to the template.
+        - Outputs the HTML that is rendered by the template.
+    """
+    mymember = Member.objects.get(id = id)
+    template = loader.get_template("details.html")
+    context = {
+        'mymember': mymember
+    }
+    return HttpResponse(template.render(context, request))
