5208766603d001b3533d16919585778e502d3375
Tejas Falke <75989281+tedy-art@users.noreply.github.com>
2023-12-21 18:35:13 +0530

    404 warning

--- my_tennis_club/members/urls.py
+++ my_tennis_club/members/urls.py
@@ -2,6 +2,7 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
+    path("", views.main, name="main"),
     path("members/", views.members, name="members"),
     path("members/details/<int:id>", views.details, name="details")
 ]
